74a9cac3ae0b7e5b3a7b5be6d84f7fabbffbae17
Collin Bazaale <collinbazaale2@gmail.com>
2021-07-04 08:23:05 -0700

    work end here

---
+++ intern.php
@@ -0,0 +1,11 @@
+<?php
+require('master.php');
+
+
+echo "<br> ====== <br> other logic in the application";
+
+$year_of_birth = 2000;
+
+$current_year = 2021;
+
+echo " <br> ==== <br> Mary is " .($current_year - $year_of_birth). " years old";
