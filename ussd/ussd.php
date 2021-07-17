af08f80be6bb1479b7e635e65698bd290141f2f8
Collin Bazaale <collinbazaale2@gmail.com>
2021-07-17 07:09:56 -0700

    ussd

--- ussd/ussd.php
+++ ussd/ussd.php
@@ -10,8 +10,6 @@ $sessionID = $_POST['sessionID'];
 
 $serviceCode = $_POST['serviceCode'];
 
-echo "CON Welcome to the Archives";
-
 $inputArray = explode("*", $textFromUser);
 
 if (empty($textFromUser)) {
