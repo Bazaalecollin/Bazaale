959604e6233ef379e1238d43d9804c41a75b666a
Collin Bazaale <collinbazaale2@gmail.com>
2021-07-17 13:13:34 -0700

    ussd

--- ussd/ussd.php
+++ ussd/ussd.php
@@ -54,22 +54,22 @@ switch ($level) {
 
 	case 3:	// 0*1*123456
 
-        $user_ID = $inputArray[2];
+           $user_ID = $inputArray[2];
          
-        $saveResident =$sqliCon->query("INSERT INTO residents(id,phone_number)VALUES('$user_ID','$phone_number')");
+            $saveResident =$sqliCon->query("INSERT INTO residents(id,phone_number)VALUES('$user_ID','$phone_number')");
 
-        if($saveResident){
+           if($saveResident){
 
         	$message = "HI".$user_ID."THANK YOU";
 
             $apikey     = "8ca31226367ab4abde28fc34a62a2ef852d0e730b66c02348c98ed7499ca087c";
 
-            $gateway    = new AfricasTalkingGateway($username, $apikey,"sandbox");
+            $gateway    = new AfricasTalkingGateway("sandbox", $apikey,"sandbox");
 
-            $gateway->sendMessage($phone_number,$message );
-        }
+            $gateway->sendMessage($phone_number, $message );
+          }
 
-             echo "END Thank you";
+          echo "END Thank you";
 
 
         }elseif ($inputArray[1] == 2) {//wants to add a city
