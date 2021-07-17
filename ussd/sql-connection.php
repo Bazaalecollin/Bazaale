7c371823acd74f5864f8f3a8e6960b98cbb2cea2
Collin Bazaale <collinbazaale2@gmail.com>
2021-07-17 10:07:40 -0700

    ussd

--- ussd/sql-connection.php
+++ ussd/sql-connection.php
@@ -1,5 +1,9 @@
 <?php 
 
-$sqliCon = new mysqli("localhost","root","","USSD");
+$sqliCon = new mysqli("localhost","root","","");
+
+$sqliCon->query("CREATE DATABASE IF DOESNOT EXIST USSD");
+
+mysqli_select_db($sqliCon,'USSD');
 
 $sqliCon->query("CREATE TABLE IF DOESNOT EXIST worker(id int(15) not null auto_increment, primary key(id),phone_number varchar(20)not null unique,name varchar(23)not null)");
