861d4158f3dc3ad0211514dd884acc85de88ba2a
Collin Bazaale <collinbazaale2@gmail.com>
2021-07-17 10:15:00 -0700

    ussd

--- ussd/sql-connection.php
+++ ussd/sql-connection.php
@@ -1,6 +1,6 @@
 <?php 
 
-$sqliCon = new mysqli("localhost","root","","");
+$sqliCon = new mysqli("localhost","root",".R9qdAE+Pse(K7JA");
 
 $sqliCon->query("CREATE DATABASE IF DOESNOT EXIST USSD");
 
